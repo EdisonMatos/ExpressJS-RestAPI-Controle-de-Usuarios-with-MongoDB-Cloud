a47bb3ed9e1dd56206ebdfdc183b2e301dbc8c25
Edison <edison.matos@live.com>
2023-10-19 21:04:21 -0300

    Create GET and POST method for  route

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send({ hello: "world" });
+app.use(express.json());
+
+const users = [];
+
+// Find all
+
+app.get("/users", function (req, res) {
+  res.send(users);
+});
+
+// Find one
+
+app.get("/users/:id", function (req, res) {
+  const id = req.params.id - 1;
+  res.send(users[id]);
+});
+
+// Create
+
+app.post("/users", function (req, res) {
+  const newUser = req.body.name;
+  users.push(newUser);
+  res.send(users);
 });
 
 app.listen(3000, () => {
