f1fc71dbad8a41e4fdb668be008f8bfa4766f11e
Edison Matos <edisonmatos@Edisons-MacBook-Pro.local>
2023-10-24 17:36:54 -0300

    Implement full crud

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const users = [];
 // Find all
 
 app.get("/users", function (req, res) {
-  res.send(users);
+  res.send(users.filter(Boolean));
 });
 
-// Find one
+// Find one (by id)
 
 app.get("/users/:id", function (req, res) {
   const id = req.params.id - 1;
@@ -24,7 +24,25 @@ app.get("/users/:id", function (req, res) {
 app.post("/users", function (req, res) {
   const newUser = req.body;
   users.push(newUser);
-  res.send(users);
+  res.send(newUser);
+});
+
+// Update
+
+app.put("/users/:id", function (req, res) {
+  const id = req.params.id - 1;
+  const newUser = req.body;
+  users[id] = newUser;
+  res.send(users[id]);
+});
+
+// Delete
+
+app.delete("/users/:id", function (req, res) {
+  const id = req.params.id - 1;
+  const index = users[id];
+  res.send(index);
+  users.splice(index, 1);
 });
 
 app.listen(3000, () => {
