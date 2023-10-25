1907c7e587dbfe7711777a13d26dcbd1773ae861
Edison Matos <edisonmatos@Edisons-MacBook-Pro.local>
2023-10-25 12:04:56 -0300

    Implement full validation

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ const users = [];
 // Find all
 
 app.get("/users", function (req, res) {
-  res.send(users.filter(Boolean));
+  if (users.length <= 0) {
+    res.send({
+      error:
+        "No users found. First you have to create a user. Try a POST request to localhost:3000/users/",
+    });
+  } else {
+    res.send({ Users: users.filter(Boolean) });
+  }
 });
 
 // Find one (by id)
@@ -17,11 +24,18 @@ app.get("/users", function (req, res) {
 app.get("/users/:id", function (req, res) {
   const id = req.params.id - 1;
 
-  if (id >= 0) {
-    console.log(typeof id);
-    res.send(users[id]);
+  if (id <= users.length - 1 && id >= 0) {
+    res.send({ UserFound: users[id] });
+  } else if (users.length == 0) {
+    res.send({
+      error:
+        "First you have to create a user. Try a POST request to localhost:3000/users/",
+    });
   } else {
-    res.send("The id must be a number. Try: users/1");
+    res.send({
+      error:
+        "The id must be a number between the quantity of users createad. Try a GET request to: localhost:3000/users/1",
+    });
   }
 });
 
@@ -31,10 +45,14 @@ app.post("/users", function (req, res) {
   const newUser = req.body;
 
   if (newUser.name == undefined || newUser.name == "") {
-    res.send('Name must be informed. Try: { "name": "Edison" } ');
+    res.send({
+      error:
+        "Name must be informed. Try a POST request with Body content in JSON format as followed:",
+      example: { name: "Edison" },
+    });
   } else {
     users.push(newUser);
-    res.send(newUser);
+    res.send({ UserCreated: newUser, message: "Createad successfully." });
   }
 });
 
@@ -42,18 +60,55 @@ app.post("/users", function (req, res) {
 
 app.put("/users/:id", function (req, res) {
   const id = req.params.id - 1;
-  const newUser = req.body;
-  users[id] = newUser;
-  res.send(users[id]);
+
+  if (id <= users.length - 1 && id >= 0) {
+    const newUser = req.body;
+
+    if (newUser.name == undefined || newUser.name == "") {
+      res.send({
+        error:
+          'Name must be informed. Try a POST request with Body content in JSON format as followed: { "name": "Edison" } ',
+      });
+    } else {
+      users[id] = newUser;
+      res.send({
+        UserUpdated: users[id],
+        message: "User updated successfully.",
+      });
+    }
+  } else if (users.length == 0) {
+    res.send({
+      error:
+        "First you have to create a user. Try a POST request to localhost:3000/users/",
+    });
+  } else {
+    res.send({
+      error:
+        "The id must be a number between the quantity of users createad. Try a GET request to: localhost:3000/users/1 to find the users list",
+    });
+  }
 });
 
 // Delete
 
 app.delete("/users/:id", function (req, res) {
   const id = req.params.id - 1;
-  const index = users[id];
-  res.send(index);
-  users.splice(index, 1);
+
+  if (id <= users.length - 1 && id >= 0) {
+    const userDeleted = users[id];
+    res.send({ userDeleted, message: "User deleted successfully." });
+    users.splice(id, 1);
+  } else if (users.length == 0) {
+    res.send({
+      error:
+        "First you have to create a user. Try a POST request to localhost:3000/users/",
+    });
+  } else {
+    res.send({
+      error:
+        "The id must be a number between the quantity of users createad. Try a GET request to: localhost:3000/users/1 to find the users list",
+    });
+  }
 });
 
 app.listen(3000, () => {
